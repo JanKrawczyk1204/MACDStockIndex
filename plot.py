e442d7ceca2c7517ac99a80d610bb93954abe34f
JanKrawczyk1204 <janekkrawczyk@poczta.fm>
2023-03-13 01:29:24 +0100

    Finished basic simulation

--- plot.py
+++ plot.py
@@ -22,5 +22,15 @@ def showPricesWithBuyAndSell(data):
     plt.plot(buy_day, buy_price, 'go', label='Buy')
     plt.plot(sell_day, sell_price, 'bo', label='Sell')
     plt.plot(data.Price, label='Price')
+    plt.xlabel("Time (Day)")
+    plt.ylabel("Price(USD)")
+    plt.title("Microsoft Stock Prices \nWith MACD calculated points")
     plt.legend()
+    plt.show()
+
+def showHistogram(days, capital_changes):
+    plt.bar(days, capital_changes)
+    plt.xlabel("Time (Day)")
+    plt.ylabel('Gain or loss (USD)')
+    plt.title('Daily gain or lost')
     plt.show()
--- simulation.py
+++ simulation.py
@@ -1,17 +1,24 @@
-
+from  plot import showHistogram
 
 def simulateStockMarket(data, capital):
     stock = 0
+    capital_over_time = []
+    days = []
+    capital_over_time.append(0)
+    days.append(0)
     starting_capital = capital
-    for i in range (1, 1000):
+    for i in range(1, 1000):
         if data.MACD[i-1] < data.Signal[i-1] and data.MACD[i] >= data.Signal[i]:
             capital, stock = buy(data, capital, stock, i)
         if data.MACD[i-1] > data.Signal[i-1] and data.MACD[i] <= data.Signal[i]:
             capital, stock = sell(data, capital, stock, i)
+        capital_over_time.append(calculateCapital(data, capital, stock, i) - calculateCapital(data, capital, stock, i-1))
+        days.append(i)
     sell(data, capital, stock, 999)
-    print('Starting capital:' + str(starting_capital))
-    print('Closing capital: ' + str(capital))
-    print('Profit:' + str(capital - starting_capital))
+    print('Starting capital:' + str(round(starting_capital, 2)))
+    print('Closing capital: ' + str(round(capital, 2)))
+    print('Profit:' + str(round(capital - starting_capital, 2)) + ' ' + str(round((capital-starting_capital)/starting_capital * 100, 2)) + '%')
+    showHistogram(days, capital_over_time)
 
 
 def buy(data, capital, stock, day):
@@ -25,3 +32,6 @@ def sell(data, capital, stock, day):
     capital += stock * data.Price[day]
     stock = 0
     return capital, stock
+
+def calculateCapital(data, capital, stock, day):
+    return capital + stock*data.Price[day]
